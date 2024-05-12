bf3441ae84616f3fae10b98832c66a93cc903d21
Daniel Dihardja <daniel@dihardja.de>
2024-05-12 13:11:23 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("...");
+});
+
+app.post("/api/v1/toolchat", async (req, res) => {
+  const question = req.body.question;
+  if (!question) {
+    return res.status(400).send("Missing question");
+  }
+  const url = `${process.env.FLOWISE_URL}/${process.env.TOOL_CHAT_ID}`;
+  return await postQuestion(url, { question });
+});
+
+app.post("/api/v1/docchat", async (req, res) => {
+  const question = req.body.question;
+  if (!question) {
+    return res.status(400).send("Missing question");
+  }
+  const url = `${process.env.FLOWISE_URL}/${process.env.DOC_CHAT_ID}`;
+  return await postQuestion(url, { question });
+});
+
+async function postQuestion(url: string, body: { question: string }) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return await response.json();
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
